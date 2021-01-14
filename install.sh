d1cf2f507c291ad82003ab7a17f092cabe8b0ceb
Zhihao Bai <zbai1@jhu.edu>
2021-01-14 15:18:39 -0500

    Add script to install the lib

---
+++ install.sh
@@ -0,0 +1,6 @@
+cp build/libcuda-control.so /lib64/libcontroller.so
+cp build/libcuda-control.so /usr/local/cuda/lib64/libcontroller.so
+cp build/libcuda-control.so /usr/local/cuda/lib64/libcuda.so
+cp build/libcuda-control.so /usr/local/cuda/lib64/libcuda.so.1
+cp build/libcuda-control.so /usr/local/cuda/lib64/libnvidia-ml.so
+cp build/libcuda-control.so /usr/local/cuda/lib64/libnvidia-ml.so.1
